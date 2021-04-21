9b146c4fa7bf018ddfd0d8ae3ba0332c520f4a34
Benjamin Movassagh <benmov@students.cs.ubc.ca>
2021-04-21 13:27:54 -0700

    -Added King Implementation

--- src/main/Pieces/King.java
+++ src/main/Pieces/King.java
@@ -6,6 +6,8 @@ import main.Piece;
 import java.awt.*;
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class King extends Piece {
 
     public King(Integer position, Board board, Boolean black) {
@@ -15,6 +17,14 @@ public class King extends Piece {
 
     @Override
     public boolean moveIsValid(Integer position) {
-        return false;
+
+        if (board.existPieceAt(position) && board.getPieceAt(position).isBlack() == black) {
+            return false;
+        }
+
+        Integer xPos = position % Board.BOARD_SIZE;
+        Integer yPos = position / Board.BOARD_SIZE;
+
+        return (abs(this.yPos - yPos) <= 1 && abs(this.xPos - xPos) <= 1);
     }
 }
